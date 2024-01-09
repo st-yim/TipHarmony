a30fe18c77b317b038028c5d4c2260244698ab95
Steven <styim@seas.upenn.edu>
2024-01-09 13:34:01 -0800

    Added splash screen (person3 image).

---
+++ TipSplitter/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  TipSplitter
+//
+//  Created by Steven Yim on 1/9/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
