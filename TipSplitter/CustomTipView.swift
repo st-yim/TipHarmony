83d9ec72245ff42399d43ccdcc93c0b01de5f4fd
Steven <styim@seas.upenn.edu>
2024-01-11 17:03:38 -0800

    Refactor: separated views and added comments.

---
+++ TipSplitter/CustomTipView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTipView.swift
+//  TipSplitter
+//
+//  Created by Steven Yim on 1/11/24.
+//
+
+import SwiftUI
+
+struct CustomTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTipView()
+}
---
+++ TipSplitter/TipButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  TipButtonView.swift
+//  TipSplitter
+//
+//  Created by Steven Yim on 1/11/24.
+//
+
+import SwiftUI
+
+struct TipButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipButtonView()
+}
